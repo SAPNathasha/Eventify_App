941d7d2a5c564eaa241481997726df70f440050e
Tharindu Wickramasinghe <131260315+SAPNathasha@users.noreply.github.com>
2024-12-28 03:16:43 +0530

    remove web view and update them with json reponses

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Eventify REST API',
+        'status' => 200
+    ]);
+});
+
+Route::fallback(function () {
+    return response()->json([
+        'message' => 'Endpoint not found',
+        'status' => 'error',
+    ], 404);
 });
