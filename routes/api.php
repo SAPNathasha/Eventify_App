f60c3f4a115aaab1189277b38abb0891968486b9
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2024-12-29 17:20:55 +0530

    Implement role based authentication

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\RoleMiddleware;
 
 Route::post('/user/register', [UserController::class, 'register']);
 Route::post('/user/login', [UserController::class, 'login']);
@@ -12,4 +13,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/event/add', [EventController::class, 'addEvent']);
     Route::put('/event/edit', [EventController::class, 'editEvent']);
     Route::delete('/event/delete', [EventController::class, 'deleteEvent']);
+});
+
+Route::middleware(['auth:sanctum', RoleMiddleware::class.':admin'])->group(function () {
+    Route::delete('/admin/delete-user/{id}', [UserController::class, 'deleteUser']);
 });
