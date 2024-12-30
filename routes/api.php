858da43ace4f1f1b293f643f184fddc223786c82
Prabhavi Nathasha <131260315+SAPNathasha@users.noreply.github.com>
2024-12-30 11:19:34 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,18 +6,32 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\RoleMiddleware;
 
+// public routes
+// user registration
 Route::post('/user/register', [UserController::class, 'register']);
+// user login
 Route::post('/user/login', [UserController::class, 'login']);
+// fetch all events
 Route::get('/event/get-all', [EventController::class, 'getAllEvents']);
 
+
+// authentication routes
 Route::middleware('auth:sanctum')->group(function () {
+    // add new event
     Route::post('/event/add', [EventController::class, 'addEvent']);
+    // edit the event
     Route::put('/event/edit', [EventController::class, 'editEvent']);
+    // delete an event
     Route::delete('/event/delete', [EventController::class, 'deleteEvent']);
 });
 
+
+// routes for admin specifications
 Route::middleware(['auth:sanctum', RoleMiddleware::class.':admin'])->group(function () {
+    // delete users by admin
     Route::delete('/admin/delete-user/{id}', [AdminController::class, 'deleteUser']);
+    // add event by admin
     Route::post('/admin/add-event', [AdminController::class, 'addEvent']);
+    // get a list of users
     Route::get('/admin/get-all-users', [AdminController::class, 'getAllUsers']);
 });
