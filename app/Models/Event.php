da3e30e03379b9521e0f3ec8ab13433efaaa96e0
Prabhavi Nathasha <131260315+SAPNathasha@users.noreply.github.com>
2024-12-30 12:46:59 +0530

    Update Event.php

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -10,16 +10,17 @@ class Event extends Model
     use HasFactory;
 
     protected $fillable = [
-        'title',
-        'description',
-        'start_time',
-        'end_time',
-        'venue',
-        'createdUserId',
+        'title',  // The title of the event
+        'description',  // A brief description of the event (optional)
+        'start_time',  // The start time of the event
+        'end_time',  // The end time of the event
+        'venue',  // The location where the event takes place
+        'createdUserId',  // The ID of the user who created the event
     ];
 
     public function creator()
     {
+        //Links this event to the user who created 'createdUserId'
         return $this->belongsTo(User::class, 'createdUserId');
     }
 }
